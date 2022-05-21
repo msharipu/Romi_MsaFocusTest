677be357731ad79337cafdae4a283e5175d772f6
malif <malif@DESKTOP-RG32ANA>
2022-05-21 16:35:39 +0800

    Add project files.

---
+++ Model/DatabaseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Romi_MsaFocusTest.Model
+{
+    public class DatabaseModel
+    {
+    }
+
+    public class Test : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ Views/UserControlScreen1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Romi_MsaFocusTest.Views
+{
+    /// <summary>
+    /// Interaction logic for UserControlScreen1.xaml
+    /// </summary>
+    public partial class UserControlScreen1 : UserControl
+    {
+        public UserControlScreen1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var myWindow = Window.GetWindow(this);
+            myWindow.Close();
+        }
+
+    }
+}
